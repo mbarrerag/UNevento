8e49871e25b2d383fb62beee9ef5010aacde1d26
MillerBarrera <mbarrerag@unal.edu.co>
2024-01-16 18:56:34 -0500

    Creating front and back

---
+++ Backend/src/test/java/com/unevento/api/ApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.unevento.api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
