03d7c5b53ff6a0d953eb1fe284877a83a32bf202
MillerBarrera <mbarrerag@unal.edu.co>
2024-02-07 21:15:44 -0500

    BCryptPasswordEncoder Login funcionando

--- Backend/src/main/java/com/unevento/api/controllers/NewUser.java
+++ Backend/src/main/java/com/unevento/api/controllers/NewUser.java
@@ -4,6 +4,7 @@ import com.unevento.api.domain.modelo.Usuario;
 import com.unevento.api.domain.records.UpdateAnswerDataUser;
 import com.unevento.api.domain.repository.UserRepository;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.util.UriComponentsBuilder;
 
@@ -14,16 +15,22 @@ import java.net.URI;
 
 public class NewUser {
     private final UserRepository userRepository;
+    private final PasswordEncoder passwordEncoder;
 
 
-    public NewUser(UserRepository userRepository) {
+    public NewUser(UserRepository userRepository, PasswordEncoder passwordEncoder) {
         this.userRepository = userRepository;
 
+        this.passwordEncoder = passwordEncoder;
     }
 
     @PostMapping
     public ResponseEntity<UpdateAnswerDataUser> newUser(@RequestBody com.unevento.api.domain.records.NewUser dataUser, UriComponentsBuilder uriBuilder) {
-      Usuario user = userRepository.save(new Usuario(dataUser));
+      String encodedPassword = passwordEncoder.encode(dataUser.contrasena());
+        Usuario user = new Usuario(dataUser);
+        user.setPassword(encodedPassword);
+        user = userRepository.save(user);
+
       UpdateAnswerDataUser answer = new UpdateAnswerDataUser(user.getId(), user.getNombre(), user.getApellido(), user.getCorreo(), user.getPassword());
         URI uri = uriBuilder.path("/getuser/{id}").buildAndExpand(user.getId()).toUri();
       return ResponseEntity.created(uri).body(answer);
--- Backend/src/main/java/com/unevento/api/domain/modelo/Usuario.java
+++ Backend/src/main/java/com/unevento/api/domain/modelo/Usuario.java
@@ -7,8 +7,11 @@ import jakarta.validation.constraints.NotBlank;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.Setter;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
 
 import java.time.LocalDateTime;
+import java.util.Collection;
 import java.util.List;
 
 @Entity
@@ -17,7 +20,7 @@ import java.util.List;
 @Setter
 @AllArgsConstructor
 
-public class Usuario {
+public class Usuario implements UserDetails {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
@@ -62,4 +65,43 @@ public class Usuario {
     }
 
 
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+     return List.of(new GrantedAuthority() {
+         @Override
+         public String getAuthority() {
+             return "ROLE_USER";
+         }
+     });
+    }
+
+    @Override
+    public String getPassword() {
+        return password;
+    }
+
+    @Override
+    public String getUsername() {
+        return nombre;
+    }
+
+    @Override
+    public boolean isAccountNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isAccountNonLocked() {
+        return true;
+    }
+
+    @Override
+    public boolean isCredentialsNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isEnabled() {
+        return true;
+    }
 }
