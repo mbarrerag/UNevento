697664133d5c9700e2aa0202efc040f8dc3ade7c
MillerBarrera <mbarrerag@unal.edu.co>
2024-03-14 19:35:00 -0500

    Perfectly Working

--- Backend/src/main/java/com/unevento/api/domain/records/GetAllEvenets.java
+++ Backend/src/main/java/com/unevento/api/domain/records/GetAllEvenets.java
@@ -6,10 +6,11 @@ import com.unevento.api.domain.modelo.Facultades;
 
 import java.util.Date;
 
-public record GetAllEvenets(String nombre, String descripcion, String lugar, Categorias categoria, Facultades Facultad, Date fechaEvento, Long capacidad, String hora, int acivo) {
+public record GetAllEvenets(Long id, String nombre, String descripcion, String lugar, Categorias categoria,
+                            Facultades Facultad, Date fechaEvento, Long capacidad, String hora, int acivo) {
 
 
     public GetAllEvenets(Eventos eventos) {
-        this(eventos.getNombre(), eventos.getDescripcion(), eventos.getLugar(), eventos.getCategoria(), eventos.getFacultad(), eventos.getFecha_evento(), eventos.getCapacidad(), eventos.getHora(), eventos.getActivo());
+        this(eventos.getId_evento(), eventos.getNombre(), eventos.getDescripcion(), eventos.getLugar(), eventos.getCategoria(), eventos.getFacultad(), eventos.getFecha_evento(), eventos.getCapacidad(), eventos.getHora(), eventos.getActivo());
     }
 }
--- Backend/src/main/java/com/unevento/api/infra/security/SecurityFilter.java
+++ Backend/src/main/java/com/unevento/api/infra/security/SecurityFilter.java
@@ -6,6 +6,8 @@ import jakarta.servlet.FilterChain;
 import jakarta.servlet.ServletException;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Component;
 import org.springframework.web.filter.OncePerRequestFilter;
 
@@ -24,7 +26,7 @@ public class SecurityFilter extends OncePerRequestFilter {
 
     @Override
     protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain) throws ServletException, IOException {
-
+        System.out.println("SecurityFilter");
         var token = request.getHeader("Authorization");
 
         if (token != null) {
@@ -38,6 +40,8 @@ public class SecurityFilter extends OncePerRequestFilter {
                 System.out.println("Token:" + jwtToken + " id:" + idLong);
                 Usuario usuario = userRepository.findByIdUsuario(idLong);
                 String subject = verificationToken.verifyRS256Token(id, jwtToken, usuario);
+                var authentication = new UsernamePasswordAuthenticationToken(usuario, null, usuario.getAuthorities());
+                SecurityContextHolder.getContext().setAuthentication(authentication);
             } catch (Exception e) {
                 throw new RuntimeException("Token inválido");
             }
