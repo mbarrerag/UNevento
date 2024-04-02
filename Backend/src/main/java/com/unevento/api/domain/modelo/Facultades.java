e7c0c806aaa7a9b802be254f9adf845e69f9873c
MillerBarrera <mbarrerag@unal.edu.co>
2024-04-01 20:56:37 -0500

    Update facultades

--- Backend/src/main/java/com/unevento/api/domain/modelo/Facultades.java
+++ Backend/src/main/java/com/unevento/api/domain/modelo/Facultades.java
@@ -3,8 +3,16 @@ package com.unevento.api.domain.modelo;
 public enum Facultades {
 
     Facultad_De_Ingenieria,
+    Facultad_De_Ciencias,
+    Facyltad_De_Ciencias_Humanas,
+    Facultad_De_Ciencias_Economicas,
     Facultad_De_Ciencias_Agrarias,
-    Facultad_De_Ciencias_De_La_Educacion,
     Facultad_De_Ciencias_De_La_Salud,
+    Facultad_De_Odontologia,
+    Facultad_De_Veterinaria,
+    Facultad_De_Derecho,
+    Facultad_De_Artes,
+    No_Aplica
+
 
 }
