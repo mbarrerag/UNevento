99ab93e5232c12314213d6549c15386ca62059cf
nmart1nezl <nmartinezl@unal.edu.co>
2024-04-24 13:40:42 -0500

    Correccion Consulta Mis Eventos

--- Backend/src/main/java/com/unevento/api/controllers/GetUserEvents.java
+++ Backend/src/main/java/com/unevento/api/controllers/GetUserEvents.java
@@ -27,7 +27,7 @@ public class GetUserEvents {
 
 
     @GetMapping("/{id}")
-    public ResponseEntity<Page<GetAllUserEvents>> getUserEvents(@PageableDefault(size = 2) Pageable pageable, @PathVariable Long id) {
+    public ResponseEntity<Page<GetAllUserEvents>> getUserEvents(@PageableDefault Pageable pageable, @PathVariable Long id) {
         Usuario usuario = userRepository.getById(id);
         Page<GetAllUserEvents> events = eventRepository.findByUsuarioCreador(usuario, pageable)
                 .map(evento -> {
