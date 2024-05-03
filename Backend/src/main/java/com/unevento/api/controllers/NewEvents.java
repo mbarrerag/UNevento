26366babab9ae3aca5e34663ea56ce66041a145c
MillerBarrera <mbarrerag@unal.edu.co>
2024-05-02 19:52:09 -0500

    Uploading images outsourcing

--- Backend/src/main/java/com/unevento/api/controllers/NewEvents.java
+++ Backend/src/main/java/com/unevento/api/controllers/NewEvents.java
@@ -1,6 +1,7 @@
 package com.unevento.api.controllers;
 
 import com.unevento.api.controllers.services.FileDeletedService;
+import com.unevento.api.controllers.services.FileService;
 import com.unevento.api.controllers.services.FileUploadService;
 import com.unevento.api.domain.modelo.Eventos;
 import com.unevento.api.domain.modelo.Usuario;
@@ -15,43 +16,53 @@ import org.springframework.web.util.UriComponentsBuilder;
 
 import java.io.IOException;
 import java.net.URI;
+import java.util.logging.Logger;
 
 @RestController
 @CrossOrigin
 @RequestMapping("/neweventun")
 
 public class NewEvents {
+    private static final Logger logger = Logger.getLogger(NewEvents.class.getName());
     private final EventRepository eventRepository;
     private final UserRepository userRepository;
-
     private final com.unevento.api.controllers.services.FileDeletedService fileDeletedService;
 
     private final com.unevento.api.controllers.services.FileUploadService fileUploadService;
 
-    public NewEvents(EventRepository eventRepository, UserRepository userRepository, FileUploadService fileUploadService, FileDeletedService fileDeletedService1) {
+    private final FileService fileService;
+
+    public NewEvents(EventRepository eventRepository, UserRepository userRepository, FileUploadService fileUploadService, FileDeletedService fileDeletedService1, FileService fileService) {
         this.eventRepository = eventRepository;
         this.userRepository = userRepository;
+
         this.fileUploadService = fileUploadService;
         this.fileDeletedService = fileDeletedService1;
+        this.fileService = fileService;
     }
 
 
     @PostMapping
     public ResponseEntity<UpdateAnswerDataEvent> creatingEvent(@RequestPart("newEvent") NewEvent newEvent, UriComponentsBuilder uriBuilder, @RequestPart(value = "file", required = false) MultipartFile file) {
+
+
         Usuario user = userRepository.getById(newEvent.userID());
         Eventos eventos;
         try {
-            String imagePath;
+
+            String nameimage;
             if (file == null || file.isEmpty()) {
                 // If no file is provided or the file is empty, assign a default image path
-                imagePath = "EventPhoto.JPG"; // Replace with your default image path
+                nameimage = "EventPhoto.JPG"; // Replace with your default image path
             } else {
                 // Use FileUploadService to handle file upload and get the path
-                imagePath = fileUploadService.uploadFile(file);
+                //imagePath = fileUploadService.uploadFile(file);
+                nameimage = fileService.upload(file);
+
 
             }
 
-            eventos = eventRepository.save(new Eventos(newEvent, user, imagePath));
+            eventos = eventRepository.save(new Eventos(newEvent, user, nameimage));
         } catch (IOException ex) {
             throw new RuntimeException(ex);
         } catch (Exception ex) {
---
+++ Backend/src/main/java/com/unevento/api/controllers/services/FileService.java
@@ -0,0 +1,71 @@
+package com.unevento.api.controllers.services;
+
+import com.google.auth.Credentials;
+import com.google.auth.oauth2.GoogleCredentials;
+import com.google.cloud.storage.*;
+import org.springframework.stereotype.Service;
+import org.springframework.web.multipart.MultipartFile;
+
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.util.UUID;
+
+@Service
+public class FileService {
+
+    private final String bucketName = "uneventophoto.appspot.com";
+    private final String pathToDownloadedJson = "Backend/src/main/resources/keys/uneventophoto-firebase-adminsdk-hmao9-efc957e87b.json";
+    private final String DOWNLOAD_URL_TEMPLATE = "https://firebasestorage.googleapis.com/v0/b/%s/o/%s?alt=media";
+
+    public String upload(MultipartFile multipartFile) throws IOException {
+        try {
+            String fileName = multipartFile.getOriginalFilename();
+            fileName = UUID.randomUUID().toString() + getExtension(fileName);
+
+            File file = convertToFile(multipartFile, fileName);
+            String downloadUrl = uploadFile(file, fileName);
+            file.delete();
+            return downloadUrl;
+        } catch (Exception e) {
+            e.printStackTrace();
+            throw new RuntimeException("Failed to upload file.");
+        }
+    }
+
+    public void download(String fileName, String destinationPath) throws IOException {
+        try {
+            Credentials credentials = GoogleCredentials.fromStream(new FileInputStream(pathToDownloadedJson));
+            Storage storage = StorageOptions.newBuilder().setCredentials(credentials).build().getService();
+            Blob blob = storage.get(BlobId.of(bucketName, fileName));
+            blob.downloadTo(Paths.get(destinationPath));
+        } catch (Exception e) {
+            e.printStackTrace();
+            throw new RuntimeException("Failed to download file.");
+        }
+    }
+
+    private String uploadFile(File file, String fileName) throws IOException {
+        BlobId blobId = BlobId.of(bucketName, fileName);
+        BlobInfo blobInfo = BlobInfo.newBuilder(blobId).setContentType("media").build();
+        Credentials credentials = GoogleCredentials.fromStream(new FileInputStream(pathToDownloadedJson));
+        Storage storage = StorageOptions.newBuilder().setCredentials(credentials).build().getService();
+        storage.create(blobInfo, Files.readAllBytes(file.toPath()));
+        return String.format(DOWNLOAD_URL_TEMPLATE, bucketName, fileName);
+    }
+
+    private File convertToFile(MultipartFile multipartFile, String fileName) throws IOException {
+        File tempFile = new File(fileName);
+        try (FileOutputStream fos = new FileOutputStream(tempFile)) {
+            fos.write(multipartFile.getBytes());
+        }
+        return tempFile;
+    }
+
+    private String getExtension(String fileName) {
+        return fileName.substring(fileName.lastIndexOf("."));
+    }
+}
